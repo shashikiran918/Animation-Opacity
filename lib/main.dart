826fe8f7e58f1c5d4fb58db7fec854841b32d92b
shashikiran918 <85386298+shashikiran918@users.noreply.github.com>
2021-06-14 15:46:28 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: AnimatedOpacityScreen(),
+    );
+  }
+}
+
+class AnimatedOpacityScreen extends StatefulWidget {
+  @override
+  _AnimatedOpacityScreenState createState() => _AnimatedOpacityScreenState();
+}
+
+class _AnimatedOpacityScreenState extends State<AnimatedOpacityScreen> {
+  double height = 250;
+  double width = 300;
+  double opacity = 0.4;
+  Color color = Colors.cyan;
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.white24,
+      appBar: AppBar(
+        title: Text("Implicit Animation Demo"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          child: Container(
+            width: width,
+            height: height,
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          opacity: opacity,
+          duration: Duration(milliseconds:600),
+          curve: Curves.easeInToLinear,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            opacity = opacity == 0.4 ? 1.0 : 0.4;
+          });
+
+        },
+        child: Icon(Icons.add,color: Colors.black,),
+      ),
+    );
+  }
+}
